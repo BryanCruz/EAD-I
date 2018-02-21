56bd61e7b5814b32caa9bceccf2f6914ef649f9a
BryanCruz <bryan.bialokur@gmail.com>
2018-02-21 17:04:39 -0300

    adição do teste de execução

--- lista 1/ex1.c
+++ lista 1/ex1.c
@@ -21,19 +21,33 @@ void listarSubsequencias(int * seq, int M, int k){
 
 }
 
+
+void testarListarSubsequencias(){
+  int seq1[] = {8,-10,4,-2,-1,0,1,2,50,51,54};
+  int M1 = 11;
+  int k1 = 4;
+
+  int seq2[] = { 5, 20, -9, 51, 52, 53, -1, -4, -3, -2, -1, 0, 1, 2, 3};
+  int M2 = 15;
+  int k2 = 7;
+
+  printf("Objetivo:\n");
+  printf("-2,-1,0,1\n");
+  printf("-1,0,1,2 \n");
+  printf("50,51    \n");
+  printf("Saída:   \n");
+  listarSubsequencias(seq1, M1, k1);
+
+  printf("\n\nObjetivo:          \n");
+  printf("51, 52, 53             \n");
+  printf("-4, -3, -2, -1, 0, 1, 2\n");
+  printf("-3, -2, -1, 0, 1, 2, 3 \n");
+  printf("Saída:\n");
+  listarSubsequencias(seq2, M2, k2);
+}
+
 int main(void){
+  testarListarSubsequencias();
 
-  //leitura dos dados para a verificação
-  int k, M;
-
-  printf("Até quantos valores consecutivos: ");
-  scanf("%d", &k);
-
-  printf("Tamanho da sequencia: ");
-  scanf("%d", &M);
-
-
-  int * seq = lerSequencia(M);
-  listarSequencia(seq, M);
   return 0;
 }
