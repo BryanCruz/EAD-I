d874f48f6fd330e1d8b9a5ad5983f7e1bc15bbbc
BryanCruz <bryan.bialokur@gmail.com>
2018-02-25 14:10:38 -0300

    função conversora finalizada

--- lista 1/ex3.c
+++ lista 1/ex3.c
@@ -1,23 +1,75 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char * converterParaHexadecimal(unsigned long long int valor){
-  return NULL;
+typedef struct s_numeroHexadecimal * numeroHexadecimal;
+struct s_numeroHexadecimal{
+  int digito;
+  numeroHexadecimal prox;
+};
+
+numeroHexadecimal converterParaHexadecimal(unsigned long long int valor){
+  numeroHexadecimal valorEmHexadecimal = NULL;
+
+  while(valor > 0){
+    numeroHexadecimal digito = (numeroHexadecimal) malloc(sizeof(struct s_numeroHexadecimal));
+
+    digito->digito = valor % 16;
+    valor = valor / 16;
+
+    digito->prox = valorEmHexadecimal;
+    valorEmHexadecimal = digito;
+  }
+  return valorEmHexadecimal;
 }
 
-void exibirValor(char * valor){
+void exibirValor(numeroHexadecimal valor){
+  while(valor != NULL){
+    if(valor->digito >= 10){
+      char c;
+      switch(valor->digito){
+        case 10:
+          c = 'A';
+          break;
+        case 11:
+          c = 'B';
+          break;
+        case 12:
+          c = 'C';
+          break;
+        case 13:
+          c = 'D';
+          break;
+        case 14:
+          c = 'E';
+          break;
+        case 15:
+          c = 'F';
+          break;
+      }
 
+      printf("%c", c);
+    }else{
+      printf("%d", valor->digito);
+    }
+    valor = valor->prox;
+  }
+  printf("\n");
 }
 
 int main(void){
-  unsigned long long int valor;
-  scanf("%llu", &valor);
+  // unsigned long long int valor;
+  // scanf("%llu", &valor);
+  //
+  // char * valorEmHexa = converterParaHexadecimal(valor);
+  //
+  // exibirValor(valorEmHexa);
+  //
+  // free(valorEmHexa);
+  //
 
-  char * valorEmHexa = converterParaHexadecimal(valor);
-
-  exibirValor(valorEmHexa);
-
-  free(valorEmHexa);
+  numeroHexadecimal valor = converterParaHexadecimal(11073303771280);
+  exibirValor(valor);
+  free(valor);
 
   return 0;
 }
