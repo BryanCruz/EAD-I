cf8aca8ce2d3c7a95598783d5fbf012b9e4c06a2
BryanCruz <bryan.bialokur@gmail.com>
2018-04-25 22:49:22 -0300

    fixed

--- sorting/mergesortligado.c
+++ sorting/mergesortligado.c
@@ -11,14 +11,13 @@ struct linkedNode{
     int grade;
     linkedNode next;
 };
-void tabular(int);
 void printList(linkedNode, linkedNode);
 linkedNode createNode(int, int);
 linkedNode insert(linkedNode *, int, int);
 void freeList(linkedNode);
 
 void mergeSort(linkedNode *, linkedNode, int, int);
-void merge(linkedNode *, linkedNode *, linkedNode, int);
+void merge(linkedNode *, linkedNode *, linkedNode, int, int);
 int isBigger(linkedNode, linkedNode);
 int isSmaller(linkedNode, linkedNode);
 linkedNode * findMiddle(linkedNode*, int);
@@ -67,71 +66,48 @@ int main(){
   return 0;
 }
 
-int top = 0;
-
-void tabular(int n){
-  for(int i = 0; i < n; i++){
-    printf("--");
-  }
-}
-
-// mergeSort from the first node to the last node before last reference
-void mergeSort(linkedNode * first, linkedNode last, int op, int listSize){
-  top++;
-  printList(*first, last);
-  if(listSize > 1){
-    tabular(top);printf("<\n");
-
-    linkedNode * middle = findMiddle(first, listSize);
-
-    mergeSort(first, *middle, op, listSize - listSize/2);
-    tabular(top);printf(">\n");
-    middle = findMiddle(first, listSize);
-
-    mergeSort(middle, last, op, listSize/2);
-
-    merge(first, middle, last, op);
-  }
-  printList(*first, last);
-  top--;
-}
-
-void merge(linkedNode * firstList, linkedNode * secList, linkedNode last, int op){
+void merge(linkedNode * firstList, linkedNode * secList, linkedNode last, int op, int firstListSize){
   //create an aux list
-tabular(top);  //printf("merge\n");
   linkedNode auxList = NULL;
   linkedNode auxLast = NULL;
 
   linkedNode tmpLeft  = *firstList;
   linkedNode tmpRight = *secList;
 	int (*compare)(linkedNode, linkedNode) = (op == 1? &isSmaller : &isBigger);
-
-  while(tmpLeft != *secList || tmpRight != last){
-    printf( "(%d %d - %d %d ", tmpLeft->ra, (*secList)->ra, tmpLeft != *secList, tmpRight != last);
-    //printf("tmpLeft=%d tmpRight=%d\n", tmpLeft->ra, tmpRight? tmpRight->ra : -1);
+  int countFirst = 0;
+  // while(tmpLeft != *secList || tmpRight != last){
+  while(countFirst < firstListSize || tmpRight != last){
     linkedNode theChosenOne;
-    if(tmpRight == last || (tmpLeft != *secList && compare(tmpLeft, tmpRight))){
+    // if(tmpRight == last || (tmpLeft != *secList && compare(tmpLeft, tmpRight))){
+    if(tmpRight == last || (countFirst < firstListSize && compare(tmpLeft, tmpRight))){
       theChosenOne = tmpLeft;
       tmpLeft = tmpLeft->next;
-
+      countFirst++;
     }else{
       theChosenOne = tmpRight;
       tmpRight = tmpRight->next;
     }
 
-    printf("adding [%d %d] ) ", theChosenOne->ra, theChosenOne->grade);
-
     if(!auxList){
       auxList    = theChosenOne;
-      *firstList = auxList;
     }else{
       auxLast->next = theChosenOne;
     }
     auxLast = theChosenOne;
   }
 
+  *firstList = auxList;
   auxLast->next = last;
-  printf("\n");
+}
+
+void mergeSort(linkedNode * first, linkedNode last, int op, int listSize){
+  if(listSize > 1){
+    linkedNode * middle = findMiddle(first, listSize);
+    mergeSort(first, *middle, op, listSize - listSize/2);
+    middle = findMiddle(first, listSize);     // ???
+    mergeSort(middle, last, op, listSize/2);
+    merge(first, middle, last, op, listSize - listSize/2);
+  }
 }
 
 int isBigger(linkedNode a, linkedNode b){
@@ -182,14 +158,11 @@ linkedNode * findMiddle(linkedNode * list, int listSize){
 
 void printList(linkedNode list, linkedNode last){
   linkedNode tmp = list;
-  tabular(top);
   printf("[LISTA]\n");
-  tabular(top);
   while(tmp != last){
-    printf("[%d %d] ", tmp->ra, tmp->grade);
+    printf("[%d %d]\n", tmp->ra, tmp->grade);
     tmp = tmp->next;
   }
-  printf("\n");
 }
 
 void freeList(linkedNode list){
