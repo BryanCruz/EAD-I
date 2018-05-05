cd87af0e3e9fe2cf5f9b95bf42af1b5c3bd7982b
BryanCruz <bryan.bialokur@gmail.com>
2018-05-05 01:05:27 -0300

    exercicio finalizado

--- desafios/quicksort.c
+++ desafios/quicksort.c
@@ -1,119 +1,217 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+struct discoVoador{
+	char * modelo;
+	int    velocidade, capacidade, prazoDeEntrega;
+	double eficiencia;
+};
 
 typedef struct linkedNode * linkedNode;
 struct linkedNode{
-	int value;
-	linkedNode next;
+	struct discoVoador valor;
+	linkedNode prox;
 };
 
-linkedNode createNode(int value){
-	linkedNode newNode = (linkedNode) malloc(sizeof(struct linkedNode));
+linkedNode criarNode(char * modelo, int velocidade, double eficiencia, int capacidade, int prazoDeEntrega){
+	linkedNode novoNode = (linkedNode) malloc(sizeof(struct linkedNode));
 
-	newNode->value = value;
-	newNode->next  = NULL;
+	novoNode->valor.modelo 		     = modelo;
+	novoNode->valor.velocidade     = velocidade;
+	novoNode->valor.capacidade     = capacidade;
+	novoNode->valor.prazoDeEntrega = prazoDeEntrega;
+	novoNode->valor.eficiencia     = eficiencia;
 
-	return newNode;
+	novoNode->prox  							 = NULL;
+
+	return novoNode;
 }
 
-linkedNode insertNode(linkedNode * list, linkedNode * last, linkedNode newNode){
-	if(*last){
-		(*last)->next = newNode;
-		*last = newNode;
+linkedNode inserirNode(linkedNode lista, linkedNode novoNode){
+	if(lista){
+		while(lista->prox) lista = lista->prox;
+		lista->prox 					   = novoNode;
+	}
+
+	return novoNode;
+}
+
+void exibirLista(linkedNode lista){
+	while(lista != NULL){
+		struct discoVoador atual = lista->valor;
+
+		printf("[");
+		printf("M=%s V=%d KM-L=%.2lf C=%d E=%d", atual.modelo, atual.velocidade, atual.eficiencia, atual.capacidade, atual.prazoDeEntrega);
+		printf("]\n");
+
+		lista = lista->prox;
+	}
+}
+
+void deletarLista(linkedNode lista){
+	if(!lista) return;
+
+	deletarLista(lista->prox);
+	free(lista->valor.modelo);
+	free(lista);
+}
+
+int compararInt(int a, int b){
+	int result;
+	if(a < b){
+		result = -1;
+	}else if(a == b){
+		result = 0;
 	}else{
-		*list = newNode;
-		*last = newNode;
+		result = 1;
 	}
 
-	return newNode;
+	return result;
 }
 
-void printList(linkedNode list, linkedNode last){
-	while(list != last){
-		printf("%d ", list->value);
-		list = list->next;
+int compararDouble(double a, double b){
+	int result;
+	if(a < b){
+		result = -1;
+	}else if(a == b){
+		result = 0;
+	}else{
+		result = 1;
 	}
-	printf("\n");
+
+	return result;
 }
 
-void deleteList(linkedNode list){
-	if(!list) return;
+int compararString(char * a, char * b){
+	int result = strcmp(a, b);
 
-	deleteList(list->next);
-	free(list);
+	return result;
 }
 
-linkedNode quickSort(linkedNode list, linkedNode last){
-	linkedNode pivo 	= list;
+int comparar(struct discoVoador a, struct discoVoador b, int modoDeComparacao){
+	int campo = modoDeComparacao/10;
+	int ordemDecrescente = modoDeComparacao%10;
 
-	linkedNode menores 	= NULL;
-	linkedNode menoresLast  = NULL;
+	int result = 0;
+	switch(campo){
+		case 1:
+			result = compararString(a.modelo, b.modelo);
+		break;
 
-	linkedNode maiores      = NULL;	
-	linkedNode maioresLast  = NULL;
+		case 2:
+			result = compararInt(a.capacidade, b.capacidade);
+		break;
 
-	linkedNode tmp 	   = pivo->next;
+		case 5:
+			result = compararInt(a.velocidade, b.velocidade);
+			if(result){
+				ordemDecrescente = 1;
+			}else{
+				result = compararDouble(a.eficiencia, b.eficiencia);
+				ordemDecrescente = !ordemDecrescente;
+			}
+		break;
 
-	while(tmp != last){
-		if(tmp->value < pivo->value){
-			if(!menores)	  menores = tmp;
-			else	menoresLast->next = tmp;
-			
-			menoresLast = tmp;
+		case 6:
+			result = compararInt(a.prazoDeEntrega, b.prazoDeEntrega);
+		break;
+	}
+
+	if(ordemDecrescente) result *= -1;
+	return result;
+}
+
+
+linkedNode quickSort(linkedNode lista, linkedNode limite, int modoDeComparacao){
+	linkedNode pivo 				    = lista;
+
+	linkedNode listaAEsquerda 	= NULL;
+	linkedNode ultimoDaEsquerda = NULL;
+
+	linkedNode listaADireita    = NULL;
+	linkedNode ultimoDaDireita  = NULL;
+
+	linkedNode rodando 	   		  = pivo->prox;
+
+	while(rodando != limite){
+		// if(rodando->valor < pivo->valor){
+		if(comparar(pivo->valor, rodando->valor, modoDeComparacao) > 0){
+			if(!listaAEsquerda) listaAEsquerda = rodando;
+			else 				ultimoDaEsquerda->prox = rodando;
+
+			ultimoDaEsquerda = rodando;
 		}else{
-			if(!maiores)      maiores = tmp;
-			else    maioresLast->next = tmp;
+			if(!listaADireita) listaADireita = rodando;
+			else      ultimoDaDireita->prox  = rodando;
 
-			maioresLast = tmp;
+			ultimoDaDireita = rodando;
 		}
-		tmp = tmp->next;		
+		rodando = rodando->prox;
 	}
 
-	if(menores){
-		menoresLast->next = pivo;
-	}
-	if(maiores){	
-		pivo->next 	  = maiores;
-		maioresLast->next = last;
+	if(listaAEsquerda != NULL){
+		ultimoDaEsquerda->prox = pivo;
+		listaAEsquerda = quickSort(listaAEsquerda, pivo, modoDeComparacao);
 	}else{
-		pivo->next = last;
+		listaAEsquerda = pivo;
 	}
-	
-	
-	if(menores    != NULL) 	menores    = quickSort(menores, pivo);
-	if(maiores    != NULL)	pivo->next = quickSort(maiores, last);
-	
-	return menores? menores : pivo;
+
+	if(listaADireita != NULL){
+		ultimoDaDireita->prox = limite;
+		pivo->prox = quickSort(listaADireita, limite, modoDeComparacao);
+	}else{
+		pivo->prox = limite;
+	}
+
+	return listaAEsquerda;
 }
 
 int main(void){
-	linkedNode list = NULL;
-	linkedNode last = NULL;
-	char op;
-	int value;
+	linkedNode lista  					= NULL;
+	linkedNode ultimoNodeInserido = NULL;
 
+	char * modelo;
+	int velocidade, capacidade, prazoDeEntrega;
+	double eficiencia;
+
+	int modoDeComparacao;
+
+	int opcao	;
 	do{
-		scanf(" %c", &op);
-		if(op >= 'a' && op <= 'z') op += 'A' - 'a';
-		printf("%c\n", op);
+		scanf("%d", &opcao);
+		switch(opcao){
+			case 1:
+			modelo = (char*) malloc(50*sizeof(char));
+				//insere um novo item no final da lista e salva a referencia para ele,
+				//agilizando a inserção de novos itens
+				scanf("%s %d %lf %d %d", modelo, &velocidade, &eficiencia, &capacidade, &prazoDeEntrega);
+				ultimoNodeInserido = inserirNode(ultimoNodeInserido, criarNode(modelo, velocidade, eficiencia, capacidade, prazoDeEntrega));
 
-		switch(op){
-			case 'I':
-				scanf("%d", &value);
-				insertNode(&list, &last, createNode(value));
+				//inicia a lista se ela estiver vazia
+				if(lista == NULL) lista = ultimoNodeInserido;
 			break;
 
-			case 'Q':
-				list = quickSort(list, NULL);
+			case 8:
+				//ordena utilizando o quick sort e um determinado modoDeComparacao de comparacao
+				scanf("%d", &modoDeComparacao);
+				lista = quickSort(lista, NULL, modoDeComparacao);
+
+				//imprime a lista
+				printf("[O=%d]\n", modoDeComparacao);
+				exibirLista(lista);
 			break;
 
-			case 'P':
-				printList(list, NULL);
+			case 2:
+				//imprime a lista
+				printf("[DISCOS]\n");
+				exibirLista(lista);
 			break;
+
 		}
 
-	}while(op != 'S');
+	}while(opcao != 0);
 
-	deleteList(list);
+	deletarLista(lista);
 	return 0;
 }
